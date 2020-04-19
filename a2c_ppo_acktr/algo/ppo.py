897b2b647300c559a2870f6f1e99fa5931fc8c43
Ilia Dobrusin <git@dobrusin.de>
2020-04-19 20:25:19 +0200

    Change logging for augmentation weight.

--- a2c_ppo_acktr/algo/ppo.py
+++ a2c_ppo_acktr/algo/ppo.py
@@ -57,6 +57,7 @@ class PPO():
         action_loss_epoch = 0
         action_loss_aug_epoch = 0
         action_loss_original_epoch = 0
+        action_loss_aug_weighted_epoch = 0
         dist_entropy_epoch = 0
         total_norm_epoch = 0
 
@@ -161,6 +162,7 @@ class PPO():
         value_loss_epoch /= num_updates
         action_loss_epoch /= num_updates
         action_loss_aug_epoch /= num_updates
+        action_loss_aug_weighted_epoch /= num_updates
         action_loss_original_epoch /= num_updates
         dist_entropy_epoch /= num_updates
         total_norm_epoch /= num_updates
