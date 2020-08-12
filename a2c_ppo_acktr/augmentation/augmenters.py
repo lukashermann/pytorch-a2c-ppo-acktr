#28, #19 Fix cosine similarity loss calculation: add mean()

a2c_ppo_acktr/augmentation/augmenters.py
@@ -128,7 +128,7 @@ class TransformsAugmenter(Augmenter):
         # Detach action_unlab to prevent the gradient flow through the network
         if self.use_cnn_loss:
             aug_loss = torch.nn.functional.cosine_similarity(cnn_output_unlab.detach(),
-                                              cnn_output_unlab_aug)
+                                              cnn_output_unlab_aug).mean()
         else:
             aug_loss = torch.nn.functional.mse_loss(action_unlab.detach(),
                                                            action_unlab_aug)