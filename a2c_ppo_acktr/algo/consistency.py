WIP: fixes in augmentation loss calculation

a2c_ppo_acktr/algo/consistency.py
@@ -105,6 +105,7 @@ class Consistency:
                 _, actor_critic_action, _, _, _ = self.actor_critic.act(obs_batch, None, None, deterministic=True)
                 with torch.no_grad():
                     _, target_action, _, _, _ = self.target_actor_critic.act(obs_batch, None, None, deterministic=True)
+
             elif self.actor_critic.return_action_probs:
                 _, _, _, _, action_probs = self.actor_critic.act(obs_batch, None, None, deterministic=True)
                 with torch.no_grad():
@@ -114,19 +115,19 @@ class Consistency:
                 # Rename for later usage in loss function
                 target_action = target_action_probs
                 actor_critic_action = action_probs
+
             else:
                 _, actor_critic_action, _, _ = self.actor_critic.act(obs_batch, None, None, deterministic=True)
                 with torch.no_grad():
                     _, target_action, _, _ = self.target_actor_critic.act(obs_batch, None, None, deterministic=True)
 
             if self.actor_critic.return_action_probs:
-                # TODO: iterate over individual losses, sum them up
                 losses = []
                 for a, t in zip(actor_critic_action, target_action):
                     losses.append(torch.nn.functional.nll_loss(torch.log(a), t))
                 action_loss = sum(losses)
             else:
-                action_loss = torch.nn.functional.mse_loss(target_action.detach(), actor_critic_action)
+                action_loss = torch.nn.functional.mse_loss(actor_critic_action, target_action.detach())
             # print("Actions: ", actor_critic_action == target_action)
 
             aug_obs_batch_orig = next(augmentation_data_loader_iter)
@@ -137,6 +138,7 @@ class Consistency:
 
             action_loss_aug, aug_obs_batch_augmented, augmenter_loss_data = self.augmenter.calculate_loss(
                 actor_critic=self.actor_critic,
+                # target_actor_critic=self.target_actor_critic,
                 obs_batch=aug_obs_batch_orig,
                 recurrent_hidden_states_batch=None,
                 masks_batch=None,
@@ -227,6 +229,7 @@ class Consistency:
 
     def update_target_critic(self, tau=1.0):
         update_state_dict(model=self.actor_critic, state_dict=self.actor_critic.state_dict(), tau=tau)
+        # update_state_dict(model=self.target_actor_critic, state_dict=self.actor_critic.state_dict(), tau=tau)
         self.target_actor_critic.eval()
         for param in self.target_actor_critic.parameters():
             param.requires_grad = False

a2c_ppo_acktr/augmentation/augmenters.py
@@ -229,10 +229,12 @@ class TransformsAugmenter(Augmenter):
                                                      kwargs["recurrent_hidden_states_batch"], \
                                                      kwargs["masks_batch"]
 
+        target_actor_critic = kwargs['target_actor_critic'] if 'target_actor_critic' in kwargs else actor_critic
+
         if self.use_cnn_loss:
             with torch.no_grad():
                 value_unlab, action_unlab, action_log_probs_unlab, rnn_hxs_unlab, cnn_output_unlab = \
-                        actor_critic.act(
+                        target_actor_critic.act(
                             obs_batch,
                             recurrent_hidden_states_batch,
                             masks_batch,
@@ -244,10 +246,11 @@ class TransformsAugmenter(Augmenter):
                     recurrent_hidden_states_batch,
                     masks_batch,
                     deterministic=True)
+
         elif self.with_actions_probs:
             with torch.no_grad():
                 value_unlab, action_unlab, action_log_probs_unlab, rnn_hxs_unlab, action_probs = \
-                    actor_critic.act(
+                    target_actor_critic.act(
                         obs_batch,
                         recurrent_hidden_states_batch,
                         masks_batch,
@@ -259,11 +262,12 @@ class TransformsAugmenter(Augmenter):
                     recurrent_hidden_states_batch,
                     masks_batch,
                     deterministic=True)
-            action_probs_aug = encode_target(action_probs_aug)
+            action_probs = encode_target(action_probs)
+
         else:
             with torch.no_grad():
                 value_unlab, action_unlab, action_log_probs_unlab, rnn_hxs_unlab = \
-                        actor_critic.act(
+                    target_actor_critic.act(
                             obs_batch,
                             recurrent_hidden_states_batch,
                             masks_batch,
@@ -288,7 +292,7 @@ class TransformsAugmenter(Augmenter):
             # aug_loss = 1 - torch.nn.functional.cosine_similarity(cnn_output_unlab.detach(), cnn_output_unlab_aug).mean()
         elif self.with_actions_probs:
             losses = []
-            for a, t in zip(action_probs, action_probs_aug):
+            for a, t in zip(action_probs_aug, action_probs):
                 losses.append(torch.nn.functional.nll_loss(torch.log(a), t))
             aug_loss = sum(losses)
         else: